34954bfa20dac78fa0ff0cba1e0c9aeaaadc729e
felipesflisboa <felipesflisboa@users.noreply.github.com>
2020-08-10 13:49:50 -0300

    chore: add git on unitytozip.bat

--- unitytozip.bat
+++ unitytozip.bat
@@ -2,5 +2,5 @@
 ECHO Enter a the file name, including extension. Valid extensions are: zip/7z
 SET /P param=
 @echo on
-7z a %param% * -r -x!Library -x!obj -x!Temp -x!.vs -x!Unused -x!unitytozip.bat -x!Assets\3rdParty\GameSparks\Resources\GameSparksSettings.asset
+7z a %param% * -r -x!Library -x!obj -x!Temp -x!.vs -x!.git -x!Unused -x!unitytozip.bat -x!Assets\3rdParty\GameSparks\Resources\GameSparksSettings.asset
 PAUSE
