14f71e4654b304253c989dcb11a32a234ab0286a
felipesflisboa <felipesflisboa@users.noreply.github.com>
2021-01-16 22:36:18 -0300

    fix: redo card Zone, to be more smooth and big

--- Assets/Scripts/Gameplay/UI/Card.cs
+++ Assets/Scripts/Gameplay/UI/Card.cs
@@ -8,11 +8,22 @@ using Gamelogic.Extensions;
 public class Card : MonoBehaviour {
     public CardType type;
     public Image image;
+    Tweener movementTween;
+    float targetLocalY = 1;
     public bool valid { get; private set; } = true;
 
-    void Awake() {
-        image.transform.localPosition = image.transform.localPosition.WithY(-1500);
-        image.transform.DOLocalMoveY(0, 0.8f).SetEase(Ease.OutCubic);
+    public RectTransform RectTransform => transform as RectTransform;
+
+    const float ANIMATION_DURATION = 0.8f;
+
+    void Start() {
+        InitialAnimation();
+    }
+
+    void InitialAnimation() {
+        DOTween.Kill(movementTween);
+        transform.localPosition = transform.localPosition.WithY(-1500);
+        movementTween = RectTransform.DOLocalMoveY(targetLocalY, ANIMATION_DURATION).SetEase(Ease.OutCubic);
     }
 
     public void Highlight() {
@@ -30,6 +41,15 @@ public class Card : MonoBehaviour {
     public void Remove() {
         valid = false;
         image.DOFade(0, 0.25f).SetEase(Ease.InSine).OnComplete(() => Destroy(gameObject));
+    }
+
+    public void RefreshPosition(float localY) {
+        if (Mathf.Approximately(targetLocalY, localY))
+            return;
+        targetLocalY = localY;
+        if (movementTween != null)
+            DOTween.Kill(movementTween);
+        movementTween = RectTransform.DOLocalMoveY(targetLocalY, ANIMATION_DURATION).SetEase(Ease.InOutSine);
     }
 
     public void OnClick() {
--- Assets/Scripts/Gameplay/UI/CardZone.cs
+++ Assets/Scripts/Gameplay/UI/CardZone.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CardZone : MonoBehaviour{
+public class CardZone : MonoBehaviour {
+    [SerializeField] RectTransform rangeRectTransform;
+    [SerializeField] float initialCardBonusY;
+    float cardHeight;
+
     public readonly List<Card> cardList = new List<Card>();
 
     public int ValidCardCount => cardList.Count(c => c.valid);
 
     public void Add(GameObject prefab) {
-        var card = Instantiate(prefab, transform).GetComponent<Card>();
-        cardList.Add(card);
+        cardList.Add(Instantiate(prefab, transform).GetComponent<Card>());
+        if (cardHeight == 0)
+            cardHeight = cardList[cardList.Count - 1].RectTransform.rect.height;
+        RefreshPosition();
     }
 
     void Update() {
         for (int i = 0; i < cardList.Count; i++) {
-            if (cardList[i] == null)
+            if (cardList[i] == null) {
                 cardList.RemoveAt(i--);
+                RefreshPosition();
+            }
+        }
+    }
+
+    void RefreshPosition() {
+        for (int i = 0; i < cardList.Count; i++) {
+            if (cardList[i] == null)
+                continue;
+            float gain = Mathf.Min(rangeRectTransform.rect.height / cardList.Count, cardHeight + initialCardBonusY);
+            cardList[i].RefreshPosition(rangeRectTransform.rect.yMax - cardHeight*0.5f - i* gain);
         }
     }
 }
