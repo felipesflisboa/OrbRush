a43dcb509ddb7c77e4500fab73bf2082cd52fe00
felipesflisboa <felipesflisboa@users.noreply.github.com>
2021-02-01 21:42:50 -0300

    fix: double cyclone effect

--- Assets/Scripts/Stage/Segment.cs
+++ Assets/Scripts/Stage/Segment.cs
@@ -135,22 +135,6 @@ public class Segment : MonoBehaviour {
         }
     }
 
-    void FixedUpdate() {
-        if (cardType == CardType.Tornado)
-            ApplyTornadoEffect();
-    }
-
-    void ApplyTornadoEffect() {
-        foreach (var player in playerInsideList) {
-            if (player.element == Element.Air)
-                continue;
-            player.rigidBody.AddForce(MathUtil.UnityAngleToNormal(
-                MathUtil.GetAngle(tornadoTransform.position.To2DXZ() - player.transform.position.To2DXZ()) - 90
-            ) * 350 * Time.deltaTime, ForceMode.Acceleration);
-        }
-
-    }
-
     void Update() {
         if(cardEffectEndTime!= 0 && cardEffectEndTime < Time.timeSinceLevelLoad)
             RemoveCardEffect();
