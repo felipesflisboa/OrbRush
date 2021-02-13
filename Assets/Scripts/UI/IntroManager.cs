b6fd58f8e9f996c9fb4f358f15cd7bc9eff81be2
felipesflisboa <felipesflisboa@users.noreply.github.com>
2021-02-13 07:52:59 -0300

    fix: game freezing on intro in WebGL

--- Assets/Scripts/UI/IntroManager.cs
+++ Assets/Scripts/UI/IntroManager.cs
@@ -1,31 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Video;
+using DG.Tweening;
 
 public class IntroManager : SingletonMonoBehaviour<IntroManager>{
     MusicController musicController;
+    SceneLoader sceneLoader;
+    VideoPlayer videoPlayer;
+    Fader fader;
 
-    async void Start(){
+    void Awake() {
         musicController = FindObjectOfType<MusicController>();
-        SceneLoader.levelName = "MainMenu"; 
-        var fader = FindObjectOfType<Fader>();
-        fader.FadeIn();
-        await new WaitWhile(() => fader.InProgress);
-        var videoPlayer = FindObjectOfType<UnityEngine.Video.VideoPlayer>();
-        videoPlayer.Prepare();
-        await new WaitWhile(() => !videoPlayer.isPrepared);
-        videoPlayer.Play();
-        await new WaitWhile(() => videoPlayer.isPlaying);
-        var loader = FindObjectOfType<SceneLoader>();
-        loader.allowSceneActivation = false;
-        loader.Activate();
-        await new WaitUntil(() => loader.loadingDone);
-        if(musicController != null)
+        fader = FindObjectOfType<Fader>(); ;
+        videoPlayer = FindObjectOfType<VideoPlayer>();
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    async void Start() {
+        await fader.FadeIn().WaitForCompletion();
+        await PlayVideo();
+        await PreloadScene();
+        if (musicController != null)
             musicController.Play();
-        fader.FadeOut();
-        await new WaitForUpdate();
-        await new WaitWhile(() => fader.InProgress);
-        await new WaitForUpdate();
-        loader.allowSceneActivation = true;
+        await fader.FadeOut().WaitForCompletion();
+        sceneLoader.allowSceneActivation = true;
+    }
+
+    async Task PlayVideo() {
+        videoPlayer.Prepare();
+#if !UNITY_WEBGL
+        await new WaitWhile(() => !videoPlayer.isPrepared);
+#endif
+        videoPlayer.Play();
+        await new WaitForSeconds(1); // WebGL won't trigger play, so gives a little time to prepare
+        await new WaitWhile(() => videoPlayer.isPlaying);
+    }
+
+    async Task PreloadScene() {
+        SceneLoader.levelName = "MainMenu";
+        sceneLoader.allowSceneActivation = false;
+        sceneLoader.Activate();
+        await new WaitUntil(() => sceneLoader.loadingDone);
     }
 }
