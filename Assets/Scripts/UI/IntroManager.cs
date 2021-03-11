2c90b3c4b91199ecf5d45ee931a5f28cafdf0dd9
felipesflisboa <felipesflisboa@users.noreply.github.com>
2021-03-11 16:40:48 -0300

    fix: Ludino video fallback on WebGL

--- Assets/Scripts/UI/IntroManager.cs
+++ Assets/Scripts/UI/IntroManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using DG.Tweening;
 
@@ -10,18 +11,23 @@ public class IntroManager : SingletonMonoBehaviour<IntroManager>{
     SceneLoader sceneLoader;
     VideoPlayer videoPlayer;
     Fader fader;
+    [SerializeField] Transform logoTransform;
+    [SerializeField] bool disableVideoOnDebug;
+
+    bool VideoPlayed => videoPlayer.time >= 0.1f;
 
     void Awake() {
         musicController = FindObjectOfType<MusicController>();
         fader = FindObjectOfType<Fader>(); ;
         videoPlayer = FindObjectOfType<VideoPlayer>();
         sceneLoader = FindObjectOfType<SceneLoader>();
+        logoTransform.gameObject.SetActive(false);
         AdvertisementHandler.I.Initialize();
     }
 
     async void Start() {
-        await fader.FadeIn().WaitForCompletion();
-        await PlayVideo();
+        fader.SetMaskAsClear();
+        await ShowLogo();
         await PreloadScene();
         if (musicController != null)
             musicController.Play();
@@ -29,12 +35,41 @@ public class IntroManager : SingletonMonoBehaviour<IntroManager>{
         sceneLoader.allowSceneActivation = true;
     }
 
-    async Task PlayVideo() {
+    async Task ShowLogo() {
         videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Videos" , "LudinoLogo.mp4");
         videoPlayer.Prepare();
         await new WaitWhile(() => !videoPlayer.isPrepared);
-        videoPlayer.Play();
+        if (!disableVideoOnDebug || !Debug.isDebugBuild)
+            videoPlayer.Play();
         await new WaitWhile(() => videoPlayer.isPlaying);
+        Debug.Log("Time="+videoPlayer.time); //remove
+        if(!VideoPlayed)
+            await ShowImageLogo();
+    }
+
+    /* //remove
+    async Task ShowLogo() {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Videos", "LudinoLogo.mp4");
+        videoPlayer.Prepare();
+        await new WaitWhile(() => !videoPlayer.isPrepared);
+        try {
+            if (disableVideoOnDebug && Debug.isDebugBuild)
+                throw new System.Exception("Debug block video play");
+            videoPlayer.Play();
+        } catch (System.Exception e) {
+            await ShowImageLogo();
+            return;
+        }
+        Debug.Log("Time=" + videoPlayer.time);
+        await new WaitWhile(() => videoPlayer.isPlaying);
+    }
+    */
+
+    async Task ShowImageLogo() {
+        fader.SetMaskAsBlack();
+        logoTransform.gameObject.SetActive(true);
+        await fader.FadeIn().WaitForCompletion();
+        await new WaitForSeconds(2f);
     }
 
     async Task PreloadScene() {
@@ -42,5 +77,16 @@ public class IntroManager : SingletonMonoBehaviour<IntroManager>{
         sceneLoader.allowSceneActivation = false;
         sceneLoader.Activate();
         await new WaitUntil(() => sceneLoader.loadingDone);
+    }
+
+    public static void PrintProperties(Object o) { //remove
+        System.Type myObjectType = o.GetType();
+        foreach (var thisVar in o.GetType().GetProperties()) {
+            try {
+                Debug.Log("o:  " + o.name + "        Var Name:  " + thisVar.Name + "         Type:  " + thisVar.PropertyType + "       Value:  " + thisVar.GetValue(o, null) + "\n");
+            } catch (System.Exception e) {
+                Debug.LogError(e);
+            }
+        }
     }
 }
