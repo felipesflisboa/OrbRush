240a34ebdeabcdbb3a1d371691b592a3e1eb4945
felipesflisboa <felipesflisboa@users.noreply.github.com>
2021-03-11 16:56:20 -0300

    chore: BlueNebula.Intro.Controller break into MainController and SpaceController

--- Assets/Scripts/Misc/BlueNebulaIntro/MainController.cs
+++ Assets/Scripts/Misc/BlueNebulaIntro/MainController.cs
@@ -4,34 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//TODO SpaceController and MainController. SpaceController is the pivot
 namespace BlueNebula.Intro {
-    public class Controller : MonoBehaviour {
-        [SerializeField] Transform pivot;
+    public class MainController : MonoBehaviour {
+        SpaceController spaceController;
         [SerializeField] Transform shootingStarTransform;
-        Vector3 shootingStarStartPos;
+        Vector3 shootingStarStartLocalPos;
         [SerializeField] AudioSource shootingStarSFX;
-        [SerializeField] SpriteRenderer spriteRenderer;
+        [SerializeField] SpriteRenderer fadeMaskRenderer;
         [SerializeField] string nextSceneName;
-        Item[] itemArray;
 
         const float INITIAL_ZOOM = 8;
 
         void Awake() {
-            shootingStarStartPos = shootingStarTransform.transform.position;
-            itemArray = GetComponentsInChildren<Item>();
-            foreach (var item in itemArray)
-                item.Setup(pivot.transform.position);
+            spaceController = GetComponentInChildren<SpaceController>(true);
+            shootingStarStartLocalPos = shootingStarTransform.transform.localPosition;
         }
 
         IEnumerator Start() {
             FadeScreen(1f);
-            RefreshItems(INITIAL_ZOOM);
+            spaceController.RefreshItems(INITIAL_ZOOM);
             yield return null;
             yield return FadeScreenRoutine(1f, true);
             yield return ZoomOutRoutine(3f);
             yield return ShootingStarRoutine(0.5f);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(1f);
             yield return FadeScreenRoutine(0.5f, false);
             if (!string.IsNullOrEmpty(nextSceneName))
                 SceneManager.LoadScene(nextSceneName);
@@ -42,21 +38,13 @@ namespace BlueNebula.Intro {
         }
 
         IEnumerator ZoomOutRoutine(float duration) {
-            return Tween(INITIAL_ZOOM, 1f, duration, RefreshItems, GetEaseOutExpo);
+            return Tween(INITIAL_ZOOM, 1f, duration, spaceController.RefreshItems, GetEaseOutExpo);
         }
 
         IEnumerator ShootingStarRoutine(float duration) {
             shootingStarSFX.Play();
             return Tween(0f, 1f, duration, MoveShootingStar, GetLinearEase);
         }
-
-        /* //remove
-        IEnumerator ZoomOut(float start, float end, float duration, float delay, Func<float, float> easeAction) {
-            RefreshItems(start);
-            yield return new WaitForSeconds(delay);
-            yield return Tween(start, end, duration, RefreshItems, easeAction);
-        }
-        */
 
         /// <summary>
         /// Simple tween
@@ -75,46 +63,18 @@ namespace BlueNebula.Intro {
                 ratioCounter = Mathf.Clamp01(ratioGain*Time.deltaTime + ratioCounter);
                 action(start + easeAction(ratioCounter)* (end-start));
             } while (ratioCounter < 1f);
-            /*//remove
-             * 
-            float step = (end - start)/ duration;
-            float counter = start;
-            do {
-                counter = Mathf.Min(end, counter + step);
-                action((counter-start)/(end-start));
-                yield return null;
-            } while (counter < end);
-             */
-        }
-
-        void RefreshItems(float zoomMultiplier) {
-            Debug.Log("zoomMultiplier="+zoomMultiplier); //remove
-            foreach (var item in itemArray)
-                item.Refresh(zoomMultiplier);
         }
 
         void MoveShootingStar(float ratio) {
-            shootingStarTransform.position = (1 - 2 * ratio) * (shootingStarStartPos - pivot.position) + pivot.position;
+            shootingStarTransform.localPosition = (1 - 2 * ratio) * shootingStarStartLocalPos;
         }
 
         void FadeScreen(float alpha) {
-            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, alpha);
-        }
-
-        float GetEaseOutQuad(float x) {
-            return 1f - (1f - x) * (1f - x);
+            fadeMaskRenderer.color = new Color(fadeMaskRenderer.color.r, fadeMaskRenderer.color.g, fadeMaskRenderer.color.b, alpha);
         }
 
         float GetEaseOutExpo(float x) {
             return x >= 1f ? 1f : 1f - Mathf.Pow(2f, -10f * x);
-        }
-
-        float GetEaseInOutExpo(float x) {
-            return x <= 0f ? 0f : (
-                x >= 1f ? 1f : (
-                    x < 0.5f ? Mathf.Pow(2f, 20f * x - 10f) / 2f : (2f - Mathf.Pow(2f, -20f * x + 10f)) / 2f
-                )
-            );
         }
 
         float GetLinearEase(float x) {
---
+++ Assets/Scripts/Misc/BlueNebulaIntro/SpaceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//TODO SpaceController and MainController. SpaceController is the pivot
+namespace BlueNebula.Intro {
+    public class SpaceController : MonoBehaviour {
+        Item[] itemArray;
+
+        void Awake() {
+            itemArray = GetComponentsInChildren<Item>();
+            foreach (var item in itemArray)
+                item.Setup(transform.position);
+        }
+
+        public void RefreshItems(float zoomMultiplier) {
+            foreach (var item in itemArray)
+                item.Refresh(zoomMultiplier);
+        }
+    }
+}
